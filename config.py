fb8ce89df926860f9fb66944f48b0b2225db43e4
DockerCS <chenzhixiang1992@gmail.com>
2019-04-15 21:52:45 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import configparser
+
+# ENV = 'dev'
+ENV = 'prod'
+
+DEV_FILE_PATH = 'v2rayU.conf'
+PROD_FILE_PATH = '/usr/local/v2rayU/v2rayU.conf'
+
+
+class Config:
+    def __init__(self):
+        #读取配置文件
+        self.config_path = eval('{}_FILE_PATH'.format(ENV.upper()))
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_path)
+
+    def get_path(self, key):
+        return self.config.get('{}-path'.format(ENV), key)
+
+    def get_web(self, key):
+        return self.config.get('web', key)
+
+    def get_data(self, key):
+        return self.config.get('data', key)
+
+    def set_data(self, key, value):
+        self.config.set('data', key, value)
+        self.config.write(open(self.config_path, "w"))
